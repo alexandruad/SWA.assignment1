e8fabef28b0188b7c929ee256cc425f922f26c05
alexandrupintican <alexandrupintican1@gmail.com>
2019-09-17 16:54:58 +0200

    File-for-second-part-of-assignment

---
+++ CourseAssignment2.js
@@ -0,0 +1,4 @@
+//Exercise 1.2 Course Assignment
+//Implement the diagram using constructors and prototypes. Use the class keyword if you like. You are not
+//required to encapsulate the fields. You need to redesign in order to deal with the multiple inheritance. Remove
+//as many redundancies as you can.
