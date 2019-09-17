a6af610aab7e1ea1687ff73d824941bf870ef1cf
alexandrupintican <alexandrupintican1@gmail.com>
2019-09-17 16:42:11 +0200

    First Commit

---
+++ CourseAssignment1.js
@@ -0,0 +1,17 @@
+//Exercise 1.1 Course Assignment
+//Implement the diagram using factory methods. Do not use constructors or the class keyword. Encapsulate
+//everything. Use concatenative inheritance to model the inheritance in the diagram, and also to remove as
+//many redundancies as you can from the diagram.
+
+function date_interval() {
+    function from() {
+        return new Date()
+    }
+    function to() {
+        return new Date()
+    }
+    function contains(date) {
+        
+    }
+}
+
