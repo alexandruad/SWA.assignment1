09a89e495e48f0b534f0c8971e42d2cac9a04d77
alexandruad04@gmail.com <D1namic2>
2019-09-22 13:30:24 +0200

    Refactored according to new information from teacher

--- CourseAssignment1.js
+++ CourseAssignment1.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 //Exercise 1.1 Course Assignment
 //Implement the diagram using factory methods. Do not use constructors or the class keyword. Encapsulate
 //everything. Use concatenative inheritance to model the inheritance in the diagram, and also to remove as
